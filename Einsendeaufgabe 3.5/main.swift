97db6f33a2ae5cf4acfe8b7458d0f29bd49fbd49
Magnus Kruschwitz <magnuskruschwitz@magnus-mbp.fritz.box>
2018-07-18 21:47:00 +0200

    Initial Commit

---
+++ Einsendeaufgabe 3.5/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Einsendeaufgabe 3.5
+//
+//  Created by Magnus Kruschwitz on 18.07.18.
+//  Copyright © 2018 Magnus Kruschwitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
